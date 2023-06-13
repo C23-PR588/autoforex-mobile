09897572a30471b7f67668dde201f52c7acaff07
shadqi.marjan <shadqi.marjan@ui.ac.id>
2023-06-13 20:08:14 +0700

    Started Working on Buy Feature

---
+++ app/src/main/java/com/c23pr588/autoforex/transaction/PurchaseActivity.kt
@@ -0,0 +1,11 @@
+package com.c23pr588.autoforex.transaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PurchaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase)
+    }
+}
